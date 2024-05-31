efb9c3062d06b93d7ca6c94356f02a7f9d7195e6
Zimin0 <you@example.com>
2024-05-31 09:49:13 +0300

    Add full crud set of routes for pcs

--- backend/apis/v1/route_pc.py
+++ backend/apis/v1/route_pc.py
@@ -1,13 +1,12 @@
+from fastapi import APIRouter, status, HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
-from fastapi import Depends, APIRouter, status
 from sqlalchemy.orm import Session
 
 from db.session import get_db
 from db.models.user import User
-from schemas.pc import PCGet, PCCreate
-from db.models.pc import PC 
+from schemas.pc import PCGet, PCCreate, PCUpdate
+from db.models.pc import PC
 from apis.v1.dependencies import get_current_active_superuser
-
 
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
@@ -18,11 +17,41 @@ async def get_all_pc(db: Session = Depends(get_db), current_user: User = Depends
     all_pcs = db.query(PC).all()
     return all_pcs
 
-@router.post('/pc', status_code=status.HTTP_201_CREATED)
+@router.post('/pc', status_code=status.HTTP_201_CREATED, response_model=PCGet)
 async def create_pc(pc: PCCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_superuser)):
-    """ POST create session. """
+    """ POST create PC. """
     db_pc = PC(physical_number=pc.physical_number, ready_to_use=pc.ready_to_use)
     db.add(db_pc)
     db.commit()
     db.refresh(db_pc)
     return db_pc
+
+@router.get('/pc/{pc_id}', response_model=PCGet)
+async def get_pc_by_id(pc_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_superuser)):
+    """ GET PC by ID. """
+    db_pc = db.query(PC).filter(PC.id == pc_id).first()
+    if db_pc is None:
+        raise HTTPException(status_code=404, detail="PC not found")
+    return db_pc
+
+@router.put('/pc/{pc_id}', response_model=PCGet)
+async def update_pc(pc_id: int, pc: PCUpdate, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_superuser)):
+    """ UPDATE PC. """
+    db_pc = db.query(PC).filter(PC.id == pc_id).first()
+    if db_pc is None:
+        raise HTTPException(status_code=404, detail="PC not found")
+    db_pc.physical_number = pc.physical_number
+    db_pc.ready_to_use = pc.ready_to_use
+    db.commit()
+    db.refresh(db_pc)
+    return db_pc
+
+@router.delete('/pc/{pc_id}')
+async def delete_pc(pc_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_superuser)):
+    """ DELETE PC. """
+    db_pc = db.query(PC).filter(PC.id == pc_id).first()
+    if db_pc is None:
+        raise HTTPException(status_code=404, detail="PC not found")
+    db.delete(db_pc)
+    db.commit()
+    return {"detail": f"PC {pc_id} was deleted."}
--- backend/schemas/pc.py
+++ backend/schemas/pc.py
@@ -14,3 +14,16 @@ class PCGet(BaseModel):
     id: int
     physical_number: int
     ready_to_use: bool
+
+    class Config:
+        orm_mode = True
+
+class PCUpdate(BaseModel):
+    physical_number: int = Field(None, description="The physical number of the PC")
+    ready_to_use: bool = Field(None, description="The availability status of the PC")
+
+    @validator("physical_number", pre=True, always=True)
+    def check_physical_number(cls, value):
+        if value is not None and value < 1:
+            raise ValueError("Physical number cannot be negative")
+        return value
