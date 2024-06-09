173b86cef9bb3ef99cdfcfd875f1a25f0f2630fd
Zimin0 <you@example.com>
2024-06-09 15:06:35 +0300

    Rewrite registration endpoint

--- backend/apis/v1/route_login.py
+++ backend/apis/v1/route_login.py
@@ -9,50 +9,36 @@ from schemas.user import UserCreate
 from db.session import get_db
 from db.repository.user import read_user_db
 from db.models.user import User
+from db.repository.user import create_user_db
 
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 
 @router.post('/register', status_code=status.HTTP_201_CREATED)
 async def register_user(user: UserCreate, db: Session = Depends(get_db)):
-    existing_user = db.query(User).filter(User.email == user.email).first()
-    if existing_user:
-        print("Такой пользователь уже существует.")
-        raise HTTPException(status_code=400, detail="User already exists")
-    
-    user_obj = User(
-        email=user.email,
-        tg_tag=user.tg_tag,
-        password=Hasher.get_password_hash(user.password),
-        is_active=True,
-        is_superuser=False
-    )
-    db.add(user_obj)
-    db.commit()
-    db.refresh(user_obj)
-    return user_obj
+    user_in_db = create_user_db(db=db, user=user)
+    ...
+    # Дополнительный функционал - например - подтверждение почты.
+    ...
+    if not user_in_db:
+        raise HTTPException(status_code=400, detail="User already exists.")
+    return user_in_db
 
 @router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = authenticate_user(form_data.username, form_data.password, db)
+    user = __authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
         )
-    access_token = create_access_token(
-        data={"sub": user.email}
-    )
-    print(access_token)
+    access_token = create_access_token(data={"sub": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
 
-def authenticate_user(email: str, password: str, db: Session):
+def __authenticate_user(email: str, password: str, db: Session):
     user = read_user_db(email=email, db=db)
-    if not user:
-        print(f"User with email {email} not found.")
+    if not user: # Пользователь не найден
         return False
-    if not Hasher.verify_password(password, user.password):
-        print(f"Incorrect password for user {email}.")
+    if not Hasher.verify_password(password, user.password): # Неверный пароль
         return False
-    print(f"Authenticated user: {user.email}")
     return user
