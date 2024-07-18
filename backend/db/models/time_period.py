9eb6ae5b1a4e00146cfc61b8359b7e974bd1dcfa
Nikita Zimenkov <work_notifications_zimino@mail.ru>
2024-07-18 22:39:36 +0300

    Implement some chaanges in Session

--- backend/db/models/time_period.py
+++ backend/db/models/time_period.py
@@ -15,24 +15,40 @@ class TimePeriod(BaseTimePeriod, Base):
     
     status = Column(Enum(StatusEnum), nullable=False, default=StatusEnum.free)
 
+    # @property.setter
+    # def status(self, value:str):
+    #     all_statuses = [stat.value for stat in StatusEnum]
+    #     if value not in all_statuses:
+    #         raise ValueError(f"Invalid status for TimePeriod. Valid are: {', '.join(all_statuses)}")
+    #     self._status = value
+
     def is_it_in_time_gap(self, start_session:time, end_session:time) -> bool:
         """ Проверяет, входит ли временной промежуток в сессию. """
         return (self.time_start >= start_session) and (self.time_end <= end_session) 
 
     @staticmethod
-    def set_status_to_time_periods(db:Session, time_periods:list, status:str, start:time, end:time):
+    def set_status_to_time_periods(db:Session, time_periods:list["TimePeriod"], status:str, start:time, end:time, mark_post_last_as_break:bool=False) -> None:
         """
         Устанавливает нужный статус всем промежутками из промежутка start:end. 
         status: [free, booked, break_between_bookings]
+        mark_post_last_as_break: поставит статус break_between_bookings промежутку следующему после последнего промежутка.
         """
         all_statuses = [stat.value for stat in StatusEnum]
         if status not in all_statuses:
             raise ValueError(f"Invalid status for TimePeriod. Valid are: {', '.join(all_statuses)}")
-        
+
+        is_session_started = False
         for period in time_periods:
             if period.is_it_in_time_gap(start, end):
+                is_session_started = True
                 period.status = status
                 db.commit()
                 db.refresh(period)
+            else:
+                if is_session_started:
+                    period.status = "break_between_bookings"
+                    db.commit()
+                    db.refresh(period)
+                    break
 
             
